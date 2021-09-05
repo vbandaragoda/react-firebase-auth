232b5a4f10ff7b9fccc627bc0628618c82676941
Vimukthi Nelanga <vimukthi.nelanga@gmail.com>
2021-09-06 00:39:20 +0530

    First one

---
+++ src/components/Signup.js
@@ -0,0 +1,70 @@
+import React, { useRef, useState } from 'react'
+import { Form, Card, Button, Alert } from 'react-bootstrap'
+import { useAuth } from '../contexts/AuthContext'
+
+export default function Signup() {
+
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
+    const { signup } = useAuth()
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+            return setError("Password do not match")
+        }
+
+        try {
+            setError('')
+            setLoading(true)
+
+            await signup(emailRef.current.value, passwordRef.current.value)
+
+        } catch {
+
+            setError("Faild to create an account")
+        }
+        setLoading(false)
+    }
+
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Sign Up</h2>
+
+                    {error && <Alert variant="danger">{error}</Alert>}
+
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group id="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control ref={emailRef} type="email" required />
+                        </Form.Group>
+
+                        <Form.Group id="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control ref={passwordRef} type="password" required />
+                        </Form.Group>
+
+                        <Form.Group id="password-confirm">
+                            <Form.Label>Password Confirmation</Form.Label>
+                            <Form.Control ref={passwordConfirmRef} type="password" required />
+                        </Form.Group>
+
+                        <Button disable={loading} className="w-100 mt-4" type="submit">Sign Up</Button>
+                    </Form>
+
+                </Card.Body>
+
+            </Card>
+            <div className="w-100 text-center mt-2">
+                Already have an account? Login
+            </div>
+        </>
+    )
+}
