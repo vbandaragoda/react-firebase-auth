7a84af228134d72d1c49139c3dddd327c08c2e45
Vimukthi Nelanga <vimukthi.nelanga@gmail.com>
2021-09-06 14:49:02 +0530

    Second one

---
+++ src/components/Dashboard.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { Button, Card, Alert } from 'react-bootstrap'
+import { useAuth } from '../contexts/AuthContext'
+
+export default function Dashboard() {
+    const [error, setError] = useState("")
+    const { currentUser } = useAuth()
+
+    function handleLogOut() {
+
+    }
+
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Profile</h2>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <strong>Email:</strong> {currentUser.email}
+                </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-2">
+                <Button variant="link" onClick="handleLogOut">Log out</Button>
+            </div>
+        </>
+    )
+}
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,13 @@
 import React, { useRef, useState } from 'react'
 import { Form, Card, Button, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
-import { Link, useHistory } from 'react-router-dom'
+import { Link, useHistory } from "react-router-dom"
 
-export default function Signup() {
+export default function Login() {
 
     const emailRef = useRef()
     const passwordRef = useRef()
-    const passwordConfirmRef = useRef()
-    const { signup } = useAuth()
+    const { login } = useAuth()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
     const history = useHistory()
@@ -17,19 +16,14 @@ export default function Signup() {
     async function handleSubmit(e) {
         e.preventDefault()
 
-        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-            return setError("Password do not match")
-        }
-
         try {
             setError("")
             setLoading(true)
-            await signup(emailRef.current.value, passwordRef.current.value)
+            await login(emailRef.current.value, passwordRef.current.value)
             history.push("/")
 
         } catch {
-
-            setError("Faild to create an account")
+            setError("Faild to sign in")
         }
         setLoading(false)
     }
@@ -38,7 +32,7 @@ export default function Signup() {
         <>
             <Card>
                 <Card.Body>
-                    <h2 className="text-center mb-4">Sign Up</h2>
+                    <h2 className="text-center mb-4">Login</h2>
 
                     {error && <Alert variant="danger">{error}</Alert>}
 
@@ -53,19 +47,14 @@ export default function Signup() {
                             <Form.Control ref={passwordRef} type="password" required />
                         </Form.Group>
 
-                        <Form.Group id="password-confirm">
-                            <Form.Label>Password Confirmation</Form.Label>
-                            <Form.Control ref={passwordConfirmRef} type="password" required />
-                        </Form.Group>
-
-                        <Button disable={loading} className="w-100 mt-4" type="submit">Sign Up</Button>
+                        <Button disable={loading} className="w-100 mt-4" type="submit">Login</Button>
                     </Form>
 
                 </Card.Body>
 
             </Card>
             <div className="w-100 text-center mt-2">
-                Already have an account? <Link to= "/login">Login</Link>
+                Need an account? <Link to="/signup">Sign Up</Link>
             </div>
         </>
     )
